3e4987fbe3573725dbf77a4bb899a02a3fc1bb7b
Torben Jost <torbenjost@proton.me>
2024-08-13 12:17:06 +0200

    add favorites listing page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "528b9fb29f3f9b6f"; }
+/******/ 		__webpack_require__.h = function() { return "bfb44658b89b669f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
